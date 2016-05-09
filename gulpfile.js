f746f151df3a2f99cd1425f8be4bf1489c9b872a
Morgan Condette <morgan.condette@gmail.com>
2016-05-09 11:28:57 -0400

    chore(): init eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp   = require('gulp'),
+    eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('lint', function () {
+    return gulp.src(['**/*.js','!node_modules/**'])
+      .pipe(eslint())
+      .pipe(eslint.formatEach())
+      .pipe(eslint.failOnError())
+      .on('error', function(error) {
+	gulpUtil.log('ESLint error: ' + error.message);
+      });
+});
+
+gulp.task('default', ['lint'],  function() {
   // default task here
 });
